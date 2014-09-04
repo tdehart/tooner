9ee6ac6f9cba1a36e0900f6f70c57c14b63f77fa
Tom DeHart <tdehart@gmail.com>
2014-09-04 18:21:39 -0400

    added swig for rendering and stubbed out initial backend-served views

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // =============================================================================
 var express = require('express');
 var bodyParser = require('body-parser');
+var swig = require('swig');
 
 var app = express();
 
@@ -15,12 +16,26 @@ mongoose.connect('mongodb://localhost/tooner-dev');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// set up static resources to be accessed at /public
+app.use(express.static(__dirname + '/public'));
+app.use('/public',  express.static(__dirname + '/bower_components'));
+
+// configure swig templating
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/app/views');
+
 // bootstrap models
 // TODO: use bootstrap code from mean to bring in all models via directory traversal
 var Toon = require('./app/models/toon');
 
 // bootstrap routes
 require('./app/routes/toons')(app);
+
+// temp index route
+app.get('/', function (req, res) {
+  res.render('index', { /* template locals context */ });
+});
 
 // START THE SERVER
 // =============================================================================
